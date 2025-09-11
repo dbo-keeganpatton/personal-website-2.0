6cb2339db1f8aabad8dfa83f3ed2979302bd0ea4
keegan patton <keeganpatton@gmail.com>
2025-09-10 22:12:36 -0500

    nothing

--- components/About.tsx
+++ components/About.tsx
@@ -4,12 +4,11 @@ export default function About() {
             <h2 className="text-3xl font-semibold mb-4">About Me</h2>
      
             <p className="text-lg">
-                Hi! I am a technologist who loves to tinker, learn new things, and is not afraid to 
-                break a couple things in the process. My experience mostly lies in Data Analytics and 
-                Engineering. I have spent several years solving complex problems, 
-                optimizing/automating processes, and building intuitive analytics 
-                solutions that tell a cohesive story, all at the 
-                highest enterpise scale.
+            sjdklsjakdjsklajdkasjkdjskajdkljsakldjaskldkjsal. djslkajdksjakjdlkas
+            dsjkaldjsklajdkjsakdjksajdkljsakljdklsajdkljsakljdkljdklsajkldjsakljdklas
+            dsjakljdksajdkjaskljdlkjaskldjlkaskldjsakljddjaslk.
+            djsklajdklsjakdjklasjdlkjaskldklasjkldjaslkjdlksajkldjklajdkl
+            djklasdjklasjdkljsakljdklasjlkdjsal.
             </p>
         
         </section>

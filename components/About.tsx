1d3a9ff2b9fcdd4bf593041ec19354ba0d339f0d
keegan patton <keeganpatton@gmail.com>
2025-09-14 20:41:40 -0500

    update about

--- components/About.tsx
+++ components/About.tsx
@@ -2,13 +2,15 @@ export default function About() {
   return (
         <section className="py-16 max-w-2xl mx-auto">
             <h2 className="text-3xl font-semibold mb-4">About Me</h2>
-     
-            <p className="text-lg">
-            sjdklsjakdjsklajdkasjkdjskajdkljsakldjaskldkjsal. djslkajdksjakjdlkas
-            dsjkaldjsklajdkjsakdjksajdkljsakljdklsajdkljsakljdkljdklsajkldjsakljdklas
-            dsjakljdksajdkjaskljdlkjaskldjlkaskldjsakljddjaslk.
-            djsklajdklsjakdjklasjdlkjaskldklasjkldjaslkjdlksajkldjklajdkl
-            djklasdjklasjdkljsakljdklasjlkdjsal.
+            
+            
+            <p className="text-2xl">
+            <span className="animate-pulse text-3xl  text-emerald-800" >❯</span>  Hello! 
+             I am a analytics engineer currently working for Walmart, and love spending time
+             playing around with databases, building pipelines, managing devops workflows, and
+             making poorly designed web apps. Below you can find a few fun projects I have done,
+             and some base documentation outlining them. I love to talk about technology, so 
+             feel free to reach out by sending me an email, or connecting over LinkedIn.
             </p>
         
         </section>

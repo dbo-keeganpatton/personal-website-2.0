9f5c026b0ef3cc64f960cfc54320b9c8b527e70b
keegan patton <keeganpatton@gmail.com>
2025-09-14 21:16:30 -0500

    add typewriter for about blurb

--- components/About.tsx
+++ components/About.tsx
@@ -1,18 +1,38 @@
+'use client'
+import dynamic from "next/dynamic";
+
+const Typewriter = dynamic(() => import("typewriter-effect"), {
+    ssr: false,
+});
+
+
 export default function About() {
-  return (
+    return (
         <section className="py-16 max-w-2xl mx-auto">
-            <h2 className="text-3xl font-semibold mb-4">About Me</h2>
-            
-            
-            <p className="text-2xl">
-            <span className="animate-pulse text-3xl  text-emerald-800" >❯</span>  Hello! 
-             I am a analytics engineer currently working for Walmart, and love spending time
-             playing around with databases, building pipelines, managing devops workflows, and
-             making poorly designed web apps. Below you can find a few fun projects I have done,
-             and some base documentation outlining them. I love to talk about technology, so 
-             feel free to reach out by sending me an email, or connecting over LinkedIn.
-            </p>
+            <h2 className="text-2xl font-semibold mb-4 pl-5">About Me</h2>
+            <div className="text-2xl flex items-start">
+                <span className="text-2xl animate-pulse text-emerald-800 mr-2">❯</span>
         
-        </section>
-    )
+                <Typewriter
+          
+                    options={{
+            
+                        strings: [
+                            `Hello! I am an analytics engineer currently working for Walmart. 
+                            I spend alot of time playing around with databases, building pipelines, 
+                            managing devops workflows, and making poorly designed web apps. 
+                            Below you can find a few fun projects I have done, and some base 
+                            documentation outlining them. I love to talk about technology, 
+                            so feel free to reach out by sending me an email, or connecting 
+                            over LinkedIn.`
+                        ],
+            autoStart: true,
+            loop: true,
+            delay: 20,
+          }}
+        />
+      </div>
+    </section>
+  );
 }
+

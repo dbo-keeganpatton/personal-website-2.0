112de8ff4f8c4332fb69fd8a9fe38b635eca7c4b
keegan patton <keeganpatton@gmail.com>
2025-09-15 21:09:13 -0500

    Complete Project 2 Modal

--- components/Project1.tsx
+++ components/Project1.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from "react"
-import Prohect1Modal from "./Project1Modal";
+import ProhectModal from "./Project1Modal";
 
 
 export default function Project1() {
@@ -25,7 +25,7 @@ export default function Project1() {
             > Learn More 
             </button>
 
-            <Prohect1Modal isOpen={modalIsOpen} onClose={handleSetModalIsOpen}>
+            <ProhectModal isOpen={modalIsOpen} onClose={handleSetModalIsOpen}>
                 <h3 className="text-2xl font-semibold mb-4 text-center">Branch Library</h3>
                 
                
@@ -86,7 +86,7 @@ export default function Project1() {
 
                 </div>
 
-           </Prohect1Modal>
+           </ProhectModal>
 
 
 
--- components/Project2.tsx
+++ components/Project2.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from "react"
-import Prohect1Modal from "./Project1Modal";
+import ProhectModal from "./Project1Modal";
 
 
 export default function Project2() {
@@ -8,8 +8,6 @@ export default function Project2() {
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const handleModalIsOpen = () => setModalIsOpen(true);
     const handleSetModalIsOpen = () => setModalIsOpen(false);
-
-    {/* Create a Modal for this project at some point */}
 
 
     return (
@@ -26,21 +24,23 @@ export default function Project2() {
             </button> 
 
             
-            <Prohect1Modal isOpen={modalIsOpen} onClose={handleSetModalIsOpen}>
+            <ProhectModal isOpen={modalIsOpen} onClose={handleSetModalIsOpen}>
                 <h3 className="text-2xl font-semibold mb-4 text-center">Dark Oura</h3>
                 
                
                 <div className="flex flex-col justify-center items-center object-center">
-                     <a 
-                        href="https://ourapi.streamlit.app/"
+                     
+                    <a  href="https://ourapi.streamlit.app/"
                         className="text-xl  border-2 rounded-md border-red-800 hover:border-emerald-700   p-1 m-3"
                     > 
                         Visit the Site 
                     </a>
-                    <p className="text-xl p-3">
-                        Automated self service analytics pipeline using the Oura Ring V2 API and Streamlit.<br/> 
-                        The user can obtain an API token from oura directly and generate using that. Secrets <br/>
-                        are used for the API call but are not stored in any way.
+
+                    <p className="text-xl p-6 w-230">
+                        Automated self service analytics pipeline using the Oura Ring V2 API and Streamlit. 
+                        The user can obtain an API token from oura directly and generate using that. Secrets 
+                        are used for the API call but are not stored 
+                        in any way.
                     </p>
 
                   
@@ -51,34 +51,26 @@ export default function Project2() {
                     />
 
                     
-                    <p className="text-xl p-3">
-                        Biometric data is extracted via a custom package I made, Ourapi.py, that standardizes<br/>
-                        programattic extraction of data from the Oura Ring V2 API. This data is then processed<br/> 
-                        using pandas and stored in memory within the Streamlit app. The data is visualized into<br/> 
-                        3 distinct categories, sleep, stress and activity. The user can define a time<br/>
+                    <p className="text-xl p-6 w-230">
+                        Biometric data is extracted via a custom package I made, Ourapi.py, that standardizes
+                        programattic extraction of data from the Oura Ring V2 API. This data is then processed 
+                        using pandas and stored in memory within the Streamlit app. The data is visualized into 
+                        3 distinct categories, sleep, stress and activity. The user can define a time
                         frame of data to pull upon extraction.                    
                     </p>
 
 
                     <img 
-                        src="/version_ctrl.png"
+                        src="/sample.png"
                         alt="site-schema"
                         width={900}
                         className="m-4 align-middle"
 
                     />
                     
-                   <img 
-                        src="/db_model.png"
-                        alt="site-schema"
-                        width={900}
-                        className="m-4 align-middle"
-
-                    />
-
                 </div>
 
-           </Prohect1Modal>
+           </ProhectModal>
 
 
 

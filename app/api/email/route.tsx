e4ac7fdf9f16bd7b77579e6c02f7fc385ab65b4a
Keegan Patton <120262579+dbo-keeganpatton@users.noreply.github.com>
2025-09-20 12:09:24 -0500

    Email flow (#9)
    * Start Email Flow
    
    * Add email API route
    
    * Complete working email | it still sucks
    
    * complete email logic

---
+++ app/api/email/route.tsx
@@ -0,0 +1,40 @@
+import { type NextRequest, NextResponse } from "next/server";
+import nodemailer from 'nodemailer';
+import Mail from "nodemailer/lib/mailer";
+
+
+export async function POST(request: NextRequest) {
+    const { email, name, message } = await request.json();
+    const transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.APPEMAIL,
+            pass: process.env.APPPASSWORD,
+        },
+    });
+
+    const mailOptions: Mail.Options = {
+        from: process.env.APPEMAIL,
+        to: process.env.APPEMAIL,
+        subject: `Message from (${email})`,
+        text: message,
+    };
+
+    const sendMailPromise = () =>
+        new Promise<string>((resolve, reject) => {
+            transport.sendMail(mailOptions, function (err) {
+                if (!err) {
+                    resolve('Thank you for reaching out, I will get back as soon as possible');
+                } else {
+                    reject(err.message);
+                }
+            });
+        });
+    
+    try {
+        await sendMailPromise();
+        return NextResponse.json({ message: 'Thank you for reaching out, I will get back as soon as possible' });
+    } catch (err) {
+        return NextResponse.json({ error: err }, { status: 500 });
+    }
+}
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,23 @@
 'use client'
-
+import { sendEmail } from "@/utils/send-email";
 import Modal from "./EmailModal";
 import { useState } from "react";
+import { FC } from "react";
+import { useForm } from "react-hook-form";
 
 
-export default function Hero() {
+export type FormData = {
+    subject: string;
+    email: string;
+    message: string;
+};
+
+
+const Hero: FC = () => {
+  const { register, handleSubmit } = useForm<FormData>();
+  function onSubmit(data: FormData) {
+    sendEmail(data);
+    }
 
     const [isModalOpen, setIsModalOpen] = useState(false);
     const handleOpenModal = () => setIsModalOpen(true);
@@ -28,24 +41,24 @@ export default function Hero() {
                 <h3 className="text-center text-1xl font-semibold mb-4 ">Leave a message after the beep</h3>
 
   
-                <form className="space-y-4">
+                <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
                     <input 
                         type="email" 
                         placeholder="Your Email" 
                         className="w-full border border-gray-300 p-2 rounded"
-                        required
+                        {...register('email', { required: true })}
                     />
                     <input 
                         type="text" 
                         placeholder="Subject" 
                         className="w-full border border-gray-300 p-2 rounded"
-                        required
+                        {...register('subject', { required: true })}
                     />
                     <textarea 
                         placeholder="Message" 
                         rows={4}
                         className="w-full border border-gray-300 p-2 rounded"
-                        required
+                        {...register('message', { required: true })}
                     />
                     <button 
                         type="submit"
@@ -58,3 +71,6 @@ export default function Hero() {
         </section>
     )
 }
+
+
+export default Hero;

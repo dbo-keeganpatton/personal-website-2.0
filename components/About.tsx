dbbb206ba0b1cf863deb55f04285a2bd479c59bb
keegan patton <keeganpatton@gmail.com>
2025-09-21 10:48:02 -0500

    Update typewriter about loop to never delete

--- components/About.tsx
+++ components/About.tsx
@@ -14,27 +14,25 @@ export default function About() {
                 <span className="lg:text-2xl sm:text-1xl animate-pulse text-emerald-800 mr-2">❯</span>
         
                 <Typewriter
-          
+                    onInit={(typewriter) => {
+                        typewriter
+                            .typeString(
+                                `Hello! I am an analytics engineer currently working for Walmart. 
+                                In my free time I spend a lot of time playing around with databases, 
+                                building pipelines, managing devops workflows, and making poorly designed 
+                                web apps. Below you can find a few fun projects I have done, and some base 
+                                documentation outlining them. I love to talk about technology, 
+                                so feel free to reach out by sending me an email, or connecting 
+                                over LinkedIn.`
+                            )
+                            .start(); 
+                    }}
                     options={{
-            
-                        strings: [
-                            `Hello! I am an analytics engineer currently working for Walmart. 
-                            In my free time I spend alot of time playing around with databases, 
-                            building pipelines, managing devops workflows, and making poorly designed 
-                            web apps. Below you can find a few fun projects I have done, and some base 
-                            documentation outlining them. I love to talk about technology, 
-                            so feel free to reach out by sending me an email, or connecting 
-                            over LinkedIn.`
-                        ],
-            
-						autoStart: true,
-						loop: true,
-						deleteSpeed: 0,
-						delay: 0,
-          
-					}}
-        
-				/>
+                        delay: 10, 
+                        autoStart: true,
+                        loop: false, 
+                    }}
+                />
       
 			</div>
     

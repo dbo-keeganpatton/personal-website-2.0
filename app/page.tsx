6b4afbe143ebbad482907a004f85e62bfd5fe32d
keegan patton <keeganpatton@gmail.com>
2025-10-11 12:21:34 -0500

    make about & hero fixed

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,14 @@ import Jobs from '@/components/Jobs'
 export default function Home() {
   
     return (
-    
+   
         <div>
-            <div className="flex flex-col lg:flex-row gap-28"> 
+            <div className="flex flex-col lg:flex-row lg:gap-28"> 
                 <div className="flex-1">
-                    <Hero />
-                    <About />
+                    <div className="fixed">
+                       <div className="fixed"> <Hero /> </div>
+                       <div className="fixed max-w-lg pt-64"> <About /> </div>
+                    </div>
                 </div>
                 <div className="flex-1 lg:pt-20">
                     <Jobs />

a6a45e14676b497624a7eaa56ac70ca54218de0d
keegan patton <keeganpatton@gmail.com>
2025-10-11 12:41:06 -0500

    correct mobile first style

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import About from '@/components/About'
 import Projects from '@/components/Projects'
 import Contact from '@/components/Contact'
 import Jobs from '@/components/Jobs'
+import EmailButton from '@/components/EmailButton'
 
 
 export default function Home() {
@@ -11,13 +12,14 @@ export default function Home() {
    
         <div>
             <div className="flex flex-col lg:flex-row lg:gap-28"> 
-                <div className="flex-1">
-                    <div className="fixed">
-                       <div className="fixed"> <Hero /> </div>
-                       <div className="fixed max-w-lg pt-64"> <About /> </div>
+                <div className="lg:flex-1">
+                    <div className="lg:fixed">
+                        <div className="lg:fixed"> <Hero /> </div>
+                        <div className="lg:fixed lg:max-w-lg lg:pt-52"> <About /> </div>
+                        <div className="lg:fixed lg:pt-120 lg:pl-40"> <EmailButton /> </div> 
                     </div>
                 </div>
-                <div className="flex-1 lg:pt-20">
+                <div className="lg:flex-1 lg:pt-20">
                     <Jobs />
                     <Projects />
                 </div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,76 +1,11 @@
 'use client'
-import { sendEmail } from "@/utils/send-email";
-import Modal from "./EmailModal";
-import { useState } from "react";
-import { FC } from "react";
-import { useForm } from "react-hook-form";
 
-
-export type FormData = {
-    subject: string;
-    email: string;
-    message: string;
+export default function Hero () {
+    return (    
+        <section className="py-20 text-center">
+            <h1 className="text-5xl font-bold animate-dust-reveal">Hi, I&#39;m Keegan...</h1>
+        </section>
+    )
 };
 
 
-const Hero: FC = () => {
-  const { register, handleSubmit } = useForm<FormData>();
-  function onSubmit(data: FormData) {
-    sendEmail(data);
-    }
-
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const handleOpenModal = () => setIsModalOpen(true);
-    const handleCloseModal = () => setIsModalOpen(false);
-
-    return (    
-        <section className="py-20 text-center">
-      
-            <h1 className="text-5xl font-bold animate-dust-reveal">Hi, I&#39;m Keegan...</h1>
-            
-            <button 
-                className="mt-6 inline-block  text-white px-6 py-2 rounded-xl border-2 border-emerald-700 hover:border-red-700"
-                onClick={handleOpenModal}
-            >
-                Get in Touch
-            </button>
-    
-
-
-            <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-                <h3 className="text-center text-1xl font-semibold mb-4 ">Leave a message after the beep</h3>
-
-  
-                <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
-                    <input 
-                        type="email" 
-                        placeholder="Your Email" 
-                        className="w-full border border-gray-300 p-2 rounded"
-                        {...register('email', { required: true })}
-                    />
-                    <input 
-                        type="text" 
-                        placeholder="Subject" 
-                        className="w-full border border-gray-300 p-2 rounded"
-                        {...register('subject', { required: true })}
-                    />
-                    <textarea 
-                        placeholder="Message" 
-                        rows={4}
-                        className="w-full border border-gray-300 p-2 rounded"
-                        {...register('message', { required: true })}
-                    />
-                    <button 
-                        type="submit"
-                        className="w-full text-white py-2 rounded hover:bg-red-700"
-                    >
-                        Send
-                    </button>
-                </form>
-            </Modal>
-        </section>
-    )
-}
-
-
-export default Hero;

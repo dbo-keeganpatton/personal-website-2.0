7f06643c5cec939147d3666f6514bb061bbbbebe
keegan patton <keeganpatton@gmail.com>
2025-10-11 12:42:58 -0500

    Remove Contact component due to redundancy

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Projects from '@/components/Projects'
-import Contact from '@/components/Contact'
 import Jobs from '@/components/Jobs'
 import EmailButton from '@/components/EmailButton'
 
@@ -24,8 +23,6 @@ export default function Home() {
                     <Projects />
                 </div>
             </div>
-            <Contact />
-
         </div>
     )
 }

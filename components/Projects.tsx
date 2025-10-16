2f176b1f1f803fafa145f92afeb81122ce3a1ba7
Keegan Patton <120262579+dbo-keeganpatton@users.noreply.github.com>
2025-10-16 11:09:24 -0500

    Add app project (#18)
    * Add personal site as project
    
    * Complete Project 5 addition

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,6 +2,7 @@ import Project1 from "./Project1"
 import Project2 from "./Project2"
 import Project3 from "./Project3"
 import Project4 from "./Project4"
+import Project5 from "./Project5"
 
 export default function Projects() {
   
@@ -9,7 +10,7 @@ export default function Projects() {
    
         <section className="lg:py-16 py-10 p-5 lg:p-0">
       
-            <h2 className="text-2xl font-semibold text-left lg:pl-5"> 
+            <h2 className="text-2xl font-semibold text-left lg:pl-5 lg:text-3xl"> 
                 Projects
                 <span className="text-emerald-500"> ❯ </span>
             </h2>
@@ -18,6 +19,7 @@ export default function Projects() {
             <div className="flex flex-col gap-4">
 
                 {/* Begin Project List */} 
+                <Project5 />
                 <Project1 />
                 <Project2 />
                 <Project3 />

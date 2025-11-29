dd9b4a03d386961a76cdba87009db89fc163bf5c
eyelady <keeganpatton@gmail.com>
2025-11-29 13:22:05 -0600

    Correct tenure

--- components/Job1.tsx
+++ components/Job1.tsx
@@ -1,38 +1,38 @@
-export default function Job1 () {
-    
-    return (
-        <div className="flex flex-col hover:border hover:border-white/5 hover:rounded-lg hover:bg-white/5 hover:text-emerald-500 lg:p-5">
-            <h1 className="whitespace-nowrap text-sm text-gray-300/80"> 2023 ➟  Present  </h1>
-            
-            <div className="flex flex-col">
-                <h1 className="text-md font-bold pb-3 lg:pr-5 text-emerald-500">
-                    <span className="text-red-500/80"> Walmart  </span>
-                    ❯ Senior Data Analyst 
-                </h1>
-                <p className="text-sm  lg:text-md text-white"> 
-                    Build and maintain executive reporting to support business-critical transformation initiatives and product analytics. 
-                    Take end-to-end ownership of data engineering and analytics processes, including architecting, building, and 
-                    maintaining production data pipelines in a cloud data warehouse. Apply data modeling principles to design optimized, 
-                    analytics-ready datasets. Research, scope, and implement emerging technologies to advance team capabilities.
-                </p>
+export default function Job1() {
 
-                {/* Tech Stack items */}
-                <div className="flex flex-wrap gap-4 pt-4">
-                    <div className="flex-initial grow-0 border-2 border-emerald-700/20 p-1 rounded-xl text-sm bg-emerald-700/20 text-center text-emerald-300">   Python         </div>
-                    <div className="flex-initial grow-0 border-2 border-emerald-700/20 p-1 rounded-xl text-sm bg-emerald-700/20 text-center text-emerald-300">   SQL            </div>
-                    <div className="flex-initial grow-0 border-2 border-emerald-700/20 p-1 rounded-xl text-sm bg-emerald-700/20 text-center text-emerald-300">   GCP            </div>
-                    <div className="flex-initial grow-0 border-2 border-emerald-700/20 p-1 rounded-xl text-sm bg-emerald-700/20 text-center text-emerald-300">   Linux          </div>
-                    <div className="flex-initial grow-0 border-2 border-emerald-700/20 p-1 rounded-xl text-sm bg-emerald-700/20 text-center text-emerald-300">   Git          </div>
+  return (
+    <div className="flex flex-col hover:border hover:border-white/5 hover:rounded-lg hover:bg-white/5 hover:text-emerald-500 lg:p-5">
+      <h1 className="whitespace-nowrap text-sm text-gray-300/80"> 2023 ➟  2025  </h1>
 
-                    <div className="flex-initial grow-0 border-2 border-emerald-700/20 p-1 rounded-xl text-sm bg-emerald-700/20 text-center text-emerald-300">   DBT            </div>
-                    <div className="flex-initial grow-0 border-2 border-emerald-700/20 p-1 rounded-xl text-sm bg-emerald-700/20 text-center text-emerald-300">   Tableau        </div>
-                    <div className="flex-initial grow-0 border-2 border-emerald-700/20 p-1 rounded-xl text-sm bg-emerald-700/20 text-center text-emerald-300">   PowerBI        </div>
-                </div>
-                {/* End Tech Stack */}
+      <div className="flex flex-col">
+        <h1 className="text-md font-bold pb-3 lg:pr-5 text-emerald-500">
+          <span className="text-red-500/80"> Walmart  </span>
+          ❯ Senior Data Analyst
+        </h1>
+        <p className="text-sm  lg:text-md text-white">
+          Build and maintain executive reporting to support business-critical transformation initiatives and product analytics.
+          Take end-to-end ownership of data engineering and analytics processes, including architecting, building, and
+          maintaining production data pipelines in a cloud data warehouse. Apply data modeling principles to design optimized,
+          analytics-ready datasets. Research, scope, and implement emerging technologies to advance team capabilities.
+        </p>
 
-            </div>
-        
+        {/* Tech Stack items */}
+        <div className="flex flex-wrap gap-4 pt-4">
+          <div className="flex-initial grow-0 border-2 border-emerald-700/20 p-1 rounded-xl text-sm bg-emerald-700/20 text-center text-emerald-300">   Python         </div>
+          <div className="flex-initial grow-0 border-2 border-emerald-700/20 p-1 rounded-xl text-sm bg-emerald-700/20 text-center text-emerald-300">   SQL            </div>
+          <div className="flex-initial grow-0 border-2 border-emerald-700/20 p-1 rounded-xl text-sm bg-emerald-700/20 text-center text-emerald-300">   GCP            </div>
+          <div className="flex-initial grow-0 border-2 border-emerald-700/20 p-1 rounded-xl text-sm bg-emerald-700/20 text-center text-emerald-300">   Linux          </div>
+          <div className="flex-initial grow-0 border-2 border-emerald-700/20 p-1 rounded-xl text-sm bg-emerald-700/20 text-center text-emerald-300">   Git          </div>
+
+          <div className="flex-initial grow-0 border-2 border-emerald-700/20 p-1 rounded-xl text-sm bg-emerald-700/20 text-center text-emerald-300">   DBT            </div>
+          <div className="flex-initial grow-0 border-2 border-emerald-700/20 p-1 rounded-xl text-sm bg-emerald-700/20 text-center text-emerald-300">   Tableau        </div>
+          <div className="flex-initial grow-0 border-2 border-emerald-700/20 p-1 rounded-xl text-sm bg-emerald-700/20 text-center text-emerald-300">   PowerBI        </div>
         </div>
+        {/* End Tech Stack */}
 
-    )
+      </div>
+
+    </div>
+
+  )
 }
